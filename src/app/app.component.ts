5c373b9c497c1ae8fccf049f4f10951d4131bca3
codevagabond <phung.linh2010@gmail.com>
2019-08-14 23:18:17 +0200

    [wip] create droppable from multiple containers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild, ElementRef } from '@angular/core';
+import { Component, ViewChild, ElementRef, ViewChildren } from '@angular/core';
 import {
   Sortable,
   Droppable,
@@ -17,7 +17,9 @@ export class AppComponent {
   droppable: Droppable;
 
   @ViewChild('dragzone', { static: false }) set initSortable(elem: ElementRef) {
-    this.sortable = new Sortable([elem.nativeElement]);
+    this.sortable = new Sortable([elem.nativeElement], {
+      draggable: '.item'
+    });
 
     this.sortable.on('sortable:start', res => {
       console.log(res, 'hahahah');
@@ -27,14 +29,16 @@ export class AppComponent {
       console.log(res.data.originalSource.id);
     });
     this.sortable.on('drag:over', res => {
-      console.log(res);
+      console.log(res, 'EHEHEHEHE');
     });
   }
 
-  @ViewChild('droppable', { static: false }) set initDroppable(
-    elem: ElementRef
-  ) {
-    this.droppable = new Droppable([elem.nativeElement]);
+  // https://github.com/Shopify/draggable/tree/master/src/Droppable
+  @ViewChildren('container') set initDroppable(elem: ElementRef) {
+    this.droppable = new Droppable([elem.nativeElement], {
+      draggable: '.item',
+      dropzone: '#dropzone'
+    });
     console.log(this.droppable);
 
     this.droppable.on('droppable:dropped', () => {
