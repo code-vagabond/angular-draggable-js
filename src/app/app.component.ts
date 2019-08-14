68df55bc8cffecd88f496c6f4c99792c2ad5392d
codevagabond <phung.linh2010@gmail.com>
2019-08-14 22:43:15 +0200

    Working on droppable zone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import {
+  Sortable,
+  Droppable,
+  DragOverContainerEvent
+} from '@shopify/draggable';
+import { LogoService } from './logo.service';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +13,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-draggable-js';
+  sortable: Sortable;
+  droppable: Droppable;
+
+  @ViewChild('dropzone', { static: false }) set initSortable(elem: ElementRef) {
+    this.sortable = new Sortable([elem.nativeElement]);
+
+    this.sortable.on('sortable:start', res => {
+      console.log(res, 'hahahah');
+    });
+    this.sortable.on('drag:over:container', (res: DragOverContainerEvent) => {
+      console.log(res, 'hihihihihi');
+      console.log(res.data.originalSource.id);
+    });
+    this.sortable.on('drag:over', res => {
+      console.log(res);
+    });
+  }
+
+  @ViewChild('droppable', { static: false }) set initDroppable(
+    elem: ElementRef
+  ) {
+    this.droppable = new Droppable([elem.nativeElement]);
+    console.log(this.droppable);
+
+    this.droppable.on('droppable:dropped', () => {
+      console.log('res');
+    });
+
+    this.droppable.on('droppable:returned', () =>
+      console.log('droppable:returned')
+    );
+  }
+
+  constructor(
+    private logoService: LogoService,
+    private sanitizer: DomSanitizer
+  ) {}
 }
